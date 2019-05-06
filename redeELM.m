e69ae5bf0462b2058aed937cf92d38c3d0ee7c33
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-05-06 14:52:36 -0300

    rede ELM aplicada com cross-validation

--- redeELM.m
+++ redeELM.m
@@ -19,3 +19,36 @@ Y = tbl(:,end);
 % Transforma o tipo tbl para arrays:
 X = table2array(X)';
 Y = table2array(Y)';
+
+%Aplicando ELM com todas as caracteristicas levantadas
+%separa o dataset em treino e teste
+for i=1:17 
+    XTreinamento = X; 
+    YTreinamento = Y;
+    for j=5:-1:1
+        XTeste(:,j) = X(:,(i-1)*5 + j); %amostras para testar o modelo
+        XTreinamento(:,(i-1)*5 + j) = []; %ficam so as amostras que vão treinar o modelo
+        YTeste(:,j) = Y(:,(i-1)*5 + j); %classes para verificar acuracia do modelo
+        YTreinamento(:,(i-1)*5 + j) = []; %ficam so as classes que vão treinar o modelo
+    end
+    %cria rede ELM com 30 camadas ocultas
+    net = newelm(X,Y,30);
+    
+    %treinando a rede com tamTreino
+    net = train(net,XTreinamento,YTreinamento);
+
+    %testando a rede com os outros tamTeste
+    yout = net(XTeste);
+    %yout = round(yout); %podemos arredondar yout e comparar com Y
+    %calcula acuracia
+    acertos = 0; %acertos
+    [~,tamTeste] = size(XTeste); %numero de testes
+    for j=1:tamTeste
+        if abs(yout(j) - YTeste(j)) < 0.9 %caso a rede neural tenha acertado
+            acertos = acertos +1; %acrescenta acertos
+        end
+    end
+    acuracia(i)=100*acertos/tamTeste; %acuracia obtida
+    str = ['Acuracia obtida: ' num2str(acuracia(i)) ' %'];
+    disp(str); %exibe a Acuracia obtida
+end
