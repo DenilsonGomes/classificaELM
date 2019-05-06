cfcfa6fb29615898bd648880ae7c96a6200083c0
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-05-06 15:18:54 -0300

    Metodo aplicado com as duas caracteristicas mais relevantes

--- redeELM.m
+++ redeELM.m
@@ -22,10 +22,10 @@ Y = table2array(Y)';
 
 %Aplicando ELM com todas as caracteristicas levantadas
 %separa o dataset em treino e teste
-for i=1:17 
+for i=1:17 %para todos os testes
     XTreinamento = X; 
     YTreinamento = Y;
-    for j=5:-1:1
+    for j=5:-1:1 %cinco elementos irão testar o modelo
         XTeste(:,j) = X(:,(i-1)*5 + j); %amostras para testar o modelo
         XTreinamento(:,(i-1)*5 + j) = []; %ficam so as amostras que vão treinar o modelo
         YTeste(:,j) = Y(:,(i-1)*5 + j); %classes para verificar acuracia do modelo
@@ -49,9 +49,49 @@ for i=1:17
         end
     end
     acuracia(i)=100*acertos/tamTeste; %acuracia obtida
-    str = ['Acuracia obtida: ' num2str(acuracia(i)) ' %'];
+    str = ['Acuracia obtida com todas as variaveis: ' num2str(acuracia(i)) ' %'];
     disp(str); %exibe a Acuracia obtida
 end
 
 str = ['Acuracia media obtida: ' num2str(mean(acuracia)) ' %'];
+disp(str); %exibe a Acuracia obtida
+
+%Aplicando ELM apenas com as duas caracteristicas mais relevantes
+%separa o dataset em treino e teste
+
+Xa(1,:) = X(1,:); %Caracteristica 1
+Xa(2,:) = X(7,:); %Caracteristica 7
+for i=1:17 % para todos os testes
+    XTreinamento = Xa; 
+    YTreinamento = Y;
+    clear XTeste;
+    for j=5:-1:1 % cinco caracteristicas testam o modelo
+        XTeste(:,j) = Xa(:,(i-1)*5 + j); %amostras para testar o modelo
+        XTreinamento(:,(i-1)*5 + j) = []; %ficam so as amostras que vão treinar o modelo
+        YTeste(:,j) = Y(:,(i-1)*5 + j); %classes para verificar acuracia do modelo
+        YTreinamento(:,(i-1)*5 + j) = []; %ficam so as classes que vão treinar o modelo
+    end
+    %cria rede ELM com 30 camadas ocultas
+    net = newelm(Xa,Y,30);
+    
+    %treinando a rede com tamTreino
+    net = train(net,XTreinamento,YTreinamento);
+
+    %testando a rede com os outros tamTeste
+    yout = net(XTeste);
+    %yout = round(yout); %podemos arredondar yout e comparar com Y
+    %calcula acuracia
+    acertos = 0; %acertos
+    [~,tamTeste] = size(XTeste); %numero de testes
+    for j=1:tamTeste
+        if abs(yout(j) - YTeste(j)) < 0.9 %caso a rede neural tenha acertado
+            acertos = acertos +1; %acrescenta acertos
+        end
+    end
+    acuracia(i)=100*acertos/tamTeste; %acuracia obtida
+    str = ['Acuracia obtida: ' num2str(acuracia(i)) ' %'];
+    disp(str); %exibe a Acuracia obtida
+end
+
+str = ['Acuracia media obtida com apenas as duas variaveis mais descritivas: ' num2str(mean(acuracia)) ' %'];
 disp(str); %exibe a Acuracia obtida
