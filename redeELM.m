2c8f67542e89d3e360528b512a7e8128e3e76d83
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-05-06 14:46:41 -0300

    Projeto iniciado!

---
+++ redeELM.m
@@ -0,0 +1,7 @@
+%Autor: Denilson Gomes Vaz da Silva
+%Graduando em Engenharia da Computação
+%Estudo sobre Evasão dp Curso de Engenharia de Computação
+
+clear %limpa as variaveis
+clc %limpa o visor 
+close all
