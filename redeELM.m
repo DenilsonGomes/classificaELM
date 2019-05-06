78a5e20a3f75ce6a5cb45edea063c72fedf8fc61
Denilson Gomes <denilsongomesdg97@gmail.com>
2019-05-06 14:48:32 -0300

    Leitura dos dados realizada

--- redeELM.m
+++ redeELM.m
@@ -5,3 +5,17 @@
 clear %limpa as variaveis
 clc %limpa o visor 
 close all
+
+% Lendo os dados:
+[fname path]=uigetfile('*.csv'); 
+fname=strcat(path,fname);
+% Preenchendo a tabela total tbl:
+tbl = readtable(fname);
+
+% Variavel dependente Y: Última coluna da tabela
+% Dados X: Todas as colunas (exceto última)
+X = tbl(:,1:end-1);
+Y = tbl(:,end);
+% Transforma o tipo tbl para arrays:
+X = table2array(X)';
+Y = table2array(Y)';
